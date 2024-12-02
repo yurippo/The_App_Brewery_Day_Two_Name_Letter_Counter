bd57e971d398e63d998c8bae4412deae66398638
Yuri Ppo <yuri.prata12@gmail.com>
2024-12-01 21:07:51 -0800

    Final Refactored Script

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
 def main():
-     
-    print(str("Number of letters in your name: ") + str(len(input("Enter your name: "))))
+     #First code
+    # print(str("Number of letters in your name: ") + str(len(input("Enter your name: "))))
 
+    #Refactored Code
+
+        name_of_the_user = input("Enter your name: ")
+        len_of_name = len(name_of_the_user)
+
+        # print(type("Number of letters in your name: ")) #str
+        # print(type(len_of_name)) #int
+
+        print("Number of letters in your name: " + str(len_of_name))
     
 
 if __name__=="__main__":
